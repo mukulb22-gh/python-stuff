64792846eb227a8536709f88c3b77d659e0482b2
mukulb22-bh <mukul.b22@gmail.com>
2025-04-29 16:17:47 +0530

    added iterator pattern

--- design_patterns/iterator.py
+++ design_patterns/iterator.py
@@ -1,4 +1,7 @@
+#Iterator design pattern with dairy productclass DairyProduct:
 
+# class DairyProduct
+class DairyProduct:
     def __init__(self, name, expiration_date):
         self.name = name
         self.expiration_date = expiration_date
@@ -6,7 +9,7 @@
     def __str__(self):
         return f"{self.name} (Expires: {self.expiration_date})"
 
-
+# class DairyIterator
 class DairyIterator:
     def __init__(self, dairy_products):
         self.dairy_products = dairy_products
@@ -24,6 +27,7 @@ class DairyIterator:
             raise StopIteration
 
 
+# class DairCollection
 class DairyCollection:
     def __init__(self):
         self.dairy_products = []
@@ -37,10 +41,11 @@ class DairyCollection:
 
 if __name__ == "__main__":
     dairy_collection = DairyCollection()
-    dairy_collection.add_product(DairyProduct("Milk", "2024-03-15"))
-    dairy_collection.add_product(DairyProduct("Yogurt", "2024-03-20"))
-    dairy_collection.add_product(DairyProduct("Cheese", "2024-04-10"))
+    dairy_collection.add_product(DairyProduct("Milk", "2025-04-15"))
+    dairy_collection.add_product(DairyProduct("Yogurt", "2025-04-20"))
+    dairy_collection.add_product(DairyProduct("Cheese", "2025-04-10"))
 
     for product in dairy_collection:
         print(product)
          examples
+
